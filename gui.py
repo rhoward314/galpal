1bafef824b09884a4a00c501de1e4f72d46eb602
sarahpopp <sep10@geneseo.edu>
2024-07-16 14:47:46 -0500

    updated structure

--- gui.py
+++ gui.py
@@ -2,70 +2,116 @@
 
 from urllib.request import urlopen
 import tkinter as tk
-from PIL import ImageTk
+from PIL import ImageTk, Image
 
+def prepare_image(image_url):
+    # display the chosen image in an existing gui window
+    # link to image
+    #img_url = image_url
+    # open image from link
+    data = urlopen(image_url)
+    # create image object
+    pil_image = Image.open(data)
+    # get original image size
+    w_old,h_old = pil_image.size
+    # size of label box for image (square)
+    box_size = 500 
+    if w_old > h_old:
+        w_new = box_size
+        h_new = int(w_new * (h_old/w_old))
+    elif h_old > w_old:
+        h_new = box_size
+        w_new = int(h_new * (w_old/h_old))
+    # resize image
+    new_pil_image = pil_image.resize((w_new,h_new))
+    tkimage = ImageTk.PhotoImage(new_pil_image)
+    # create label widget in gui with image
+    return tkimage
 
-# this is the code i used to experiment with gui - delete later
-# basic code to open an image from the internet
-#image_url = "https://science.nasa.gov/wp-content/uploads/2023/04/heic1901a-jpg.webp?w=2048&format=webp"
+def test_label(text,gui_window):
+    tk.Label(gui_window,text=text).pack()
 
-#root = tk.Tk()
-#data = urlopen(image_url)
-#image = ImageTk.PhotoImage(data=data.read())
-#tk.Label(root, image=image).pack()
-#root.mainloop()
-
-# create a GUI window
-#root = tk.Tk()
-#root.title('this is a title')
-# things in the window will be added later
-
-# add a label
-#w_label = tk.Label(root, text='this is a label').grid(row=0)
-#w_label.pack()
-
-# add a button
-#w_button = tk.Button(root,text='button', width=25,command=root.destroy).grid(row=1)
-#w_button.pack()
-
-# add input field
-#tk.Label(root, text='input 1').grid(row=2)
-#tk.Label(root, text='input 2').grid(row=3)
-#e1 = tk.Entry(root).grid(row=2, column=1)
-#e2 = tk.Entry(root).grid(row=3, column=1)
-
-# add checkboxes
-#tk.Checkbutton(root,text='option 1').grid(row=4)
-#tk.Checkbutton(root,text='option 2').grid(row=4, column=1)
-
-# this opens the window and keeps it open until you close it
-#root.mainloop()
-
-
-# actual code for the project starts here
-
-# image link (will eventually pull from a list of links)
-image_url = "https://science.nasa.gov/wp-content/uploads/2023/04/heic1901a-jpg.webp?w=2048&format=webp"
 
 # create a GUI window
 root = tk.Tk()
+# set title for window
 root.title('Gal Pal Galaxy Classification')
+# set size of window
 root.geometry('800x600')
 
-# add image
-#data = urlopen(image_url)
-#resized_image = data.resize(('600,400'))
-#image = ImageTk.PhotoImage(data=data.read())
-#resized_image = image.resize(('600,400'))
-#tk.Label(root, image=image).pack()
-
-# different way to add image
-canvas= tk.Canvas(root, width= 600, height= 400)
-canvas.pack()
+'''
+# image link (will eventually pull from a list of links)
+image_url = "https://science.nasa.gov/wp-content/uploads/2023/04/heic1901a-jpg.webp?w=2048&format=webp"
+# open image from link
 data = urlopen(image_url)
-image = ImageTk.PhotoImage(data=data.read())
-#resized_image= image.resize((300,205), Image.ANTIALIAS)
-#new_image= ImageTk.PhotoImage(resized_image)
-canvas.create_image(10,10, anchor=tk.NW, image=image)
 
+# create image object
+pil_image = Image.open(data)
+# get original image size
+w_old,h_old = pil_image.size
+
+# size of label box for image (square)
+box_size = 500 
+
+if w_old > h_old:
+    w_new = box_size
+    h_new = int(w_new * (h_old/w_old))
+elif h_old > w_old:
+    h_new = box_size
+    w_new = int(h_new * (w_old/h_old))
+
+# resize image
+new_pil_image = pil_image.resize((w_new,h_new))
+image = ImageTk.PhotoImage(new_pil_image)
+
+# create label widget in gui with image
+tk.Label(root, image=image, width=500,height=500,bg='lightgray').pack(pady=20)'''
+
+image_url = "https://science.nasa.gov/wp-content/uploads/2023/04/heic1901a-jpg.webp?w=2048&format=webp"
+#display_image(url1,root)
+
+data = urlopen(image_url)
+
+# create image object
+pil_image = Image.open(data)
+# get original image size
+w_old,h_old = pil_image.size
+
+# size of label box for image (square)
+box_size = 500 
+
+if w_old > h_old:
+    w_new = box_size
+    h_new = int(w_new * (h_old/w_old))
+elif h_old > w_old:
+    h_new = box_size
+    w_new = int(h_new * (w_old/h_old))
+
+# resize image
+new_pil_image = pil_image.resize((w_new,h_new))
+image = ImageTk.PhotoImage(new_pil_image)
+
+# create label widget in gui with image
+#image_label = tk.Label(root, image=image, width=500,height=500,bg='lightgray')
+#image_label.pack(pady=20)
+
+# create label with box for image
+image_label = tk.Label(root, width=500,height=500,bg='lightgray')
+image_label.pack(pady=20)
+
+image1 = prepare_image(image_url)
+image_label.configure(image=image1)
+
+test_label('this is a label',root)
+
+label2 = tk.Label(root,text='this is a different label')
+label2.pack()
+label2.configure(text='and even more labels')
+
+url2 = 'https://science.nasa.gov/wp-content/uploads/2023/04/m51-and-companion_0-jpg.webp?w=2048&format=webp'
+
+image2 = prepare_image(url2)
+image_label.configure(image=image2)
+
+# keeps gui window open until you close it
 root.mainloop()

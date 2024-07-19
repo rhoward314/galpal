a493f7c2d54abb70d5729af3c9f76441efb3dacf
Robert Howard <rhoward31415926535@gmail.com>
2024-07-18 20:19:32 -0500

    Formatting changes in gui.py, added main.py

--- galpal/gui.py
+++ galpal/gui.py
@@ -67,14 +67,14 @@ def get_info_text(gal_obj):
     # spiral text
     if gal_obj.morph_type == 'spiral':
         info_text = str(gal_obj.name) + ' is a spiral galaxy! '
-        info_text += 'It is located ' + str(gal_obj.distance) +' light years away in the ' + str(gal_obj.constellation)+'. '
+        info_text += 'It is located ' + str(gal_obj.distance) + ' light years away in the ' + str(gal_obj.constellation) + '. '
         info_text += 'It has a stellar mass that is ' + str(gal_obj.stellar_mass) + ' times greater than our sun. '
-        info_text += 'This is a star-forming galaxy with a star-formation rate of roughly ' + str(gal_obj.star_formation) +' solar masses per year! '
+        info_text += 'This is a star-forming galaxy with a star-formation rate of roughly ' + str(gal_obj.star_formation) + ' solar masses per year! '
         info_text += 'For comparison, the Milky Way forms stars at a rate of roughly one solar mass per year.'
     # elliptical text
     elif gal_obj.morph_type == 'elliptical':
         info_text = str(gal_obj.name) + 'is an elliptical galaxy! '
-        info_text += 'It is located ' +str(gal_obj.distance) + ' light years away in the ' + str(gal_obj.constellation)+'. '
+        info_text += 'It is located ' +str(gal_obj.distance) + ' light years away in the ' + str(gal_obj.constellation) + '. '
         info_text += 'It has a stellar mass that is ' + str(gal_obj.stellar_mass) + 'times greater than our sun. '
         info_text += 'It contains an old stellar population, and it is not actively forming stars.'
     else:
@@ -84,33 +84,33 @@ def get_info_text(gal_obj):
 
 
 # check if user selection of spiral or elliptical is correct
-def is_correct(gui_obj,gal_obj,score_label):
+def is_correct(gui_obj, gal_obj, score_label):
     gui_obj.attempts += 1
     if gal_obj.choice == gal_obj.morph_type:
         gal_obj.grade = 1
         gui_obj.grade += 1
-        score_text = str(gui_obj.grade) + ' correct out of ' + str(gui_obj.attempts)+ ' attempts.'
-        score_label.configure(text=score_text,width=25,wraplength=200)
+        score_text = str(gui_obj.grade) + ' correct out of ' + str(gui_obj.attempts) + ' attempts.'
+        score_label.configure(text=score_text, width=25, wraplength=200)
         return 'That is correct!'
     else:
         gal_obj.grade = 0
-        score_text = str(gui_obj.grade) + ' correct out of ' + str(gui_obj.attempts)+ ' attempts.'
-        score_label.configure(text=score_text,width=25,wraplength=200)
+        score_text = str(gui_obj.grade) + ' correct out of ' + str(gui_obj.attempts) + ' attempts.'
+        score_label.configure(text=score_text, width=25, wraplength=200)
         return 'That is incorrect.'
 
 # make button function
-def spiral_func(gui_obj,gal_objs,info_label,score_label):
+def spiral_func(gui_obj, gal_objs, info_label, score_label):
     galaxy_obj = gal_objs[gui_obj.which_gal]
     galaxy_obj.choice = 'spiral'
-    label_text = 'You selected spiral.\n' + is_correct(gui_obj,galaxy_obj,score_label)
+    label_text = 'You selected spiral.\n' + is_correct(gui_obj, galaxy_obj, score_label)
     label_text += '\n\n'
     label_text += get_info_text(galaxy_obj)
     info_label.configure(text=label_text)
 
-def elliptical_func(gui_obj,gal_objs,info_label,score_label):
+def elliptical_func(gui_obj, gal_objs, info_label, score_label):
     galaxy_obj = gal_objs[gui_obj.which_gal]
     galaxy_obj.choice = 'elliptical'
-    label_text = 'You selected elliptical.\n' + is_correct(gui_obj,galaxy_obj,score_label)
+    label_text = 'You selected elliptical.\n' + is_correct(gui_obj, galaxy_obj, score_label)
     label_text += '\n\n'
     label_text += get_info_text(galaxy_obj)
     info_label.configure(text=label_text)
@@ -150,22 +150,21 @@ def next_gal(gui_obj, gal_objs, link_df, desc_df, image_label, info_label, galax
     image_label.image = new_image
     galaxy_option.set(options[gui_obj.which_gal])
 
-def dropdown_select_gal(selection,gui_obj, gal_objs, link_df, desc_df, image_label):
+def dropdown_select_gal(selection, gui_obj, gal_objs, link_df, desc_df, image_label):
     #info_label.configure(text='')  <-- this will only work if dropdown menu is set up after the info box
     # but right now dropdown menu is set up first and info box placement depends on dropdown placement
     # this could be changed but i'm not going to do it now
     print(selection)
     sel_list = selection.split()
     print(sel_list)
-    gui_obj.which_gal = int(sel_list[-1])-1
+    gui_obj.which_gal = int(sel_list[-1]) - 1
     print(gui_obj.which_gal)
     update_gal(gui_obj, gal_objs[gui_obj.which_gal], link_df, desc_df)
     new_image = prepare_image(gui_obj.url)
     image_label.configure(image=new_image)
     image_label.image = new_image
 
-def main():
-
+def setup_gui():
     # create galaxy object (this will probably go in main.py?)
     #current_gal = Galaxy.Galaxy(0,0,0,0,0,0)
 
@@ -200,7 +199,7 @@ def main():
     galaxy_option = tk.StringVar(root)
     galaxy_option.set(options[0])
 
-    dropdown = tk.OptionMenu(root, galaxy_option, *options, command = partial(dropdown_select_gal,gui_obj=gui_obj, gal_objs=gal_objs, link_df=link_df, desc_df=desc_df, image_label=image_label))
+    dropdown = tk.OptionMenu(root, galaxy_option, *options, command = partial(dropdown_select_gal, gui_obj=gui_obj, gal_objs=gal_objs, link_df=link_df, desc_df=desc_df, image_label=image_label))
     dropdown.place(x=0, y=0)
     dropdown.update()
     dropdown_width, dropdown_height = dropdown.winfo_width(), dropdown.winfo_height()
@@ -209,31 +208,31 @@ def main():
     dropdown.place(x=(margin_width - dropdown_width) / 2, y=dropdown_y)
 
     # create label for info box
-    info_label = tk.Label(root,text='',width=25,wraplength=200)
-    info_label.place(x=0,y=0)
+    info_label = tk.Label(root,text='', width=25, wraplength=200)
+    info_label.place(x=0, y=0)
     info_label_width = info_label.winfo_width()
     #info_label.place_forget()
     #info_label_x = (margin_width - info_label_width)/2
     info_label_y = dropdown_y + dropdown_height + 200
-    info_label.place(x=15,y=info_label_y)
+    info_label.place(x=15, y=info_label_y)
 
     # create label for score
-    score_text = str(gui_obj.grade) + ' correct out of ' + str(gui_obj.attempts)+ ' attempts.'
-    score_label = tk.Label(root, text=score_text,width=25,wraplength=200)
-    score_label.place(x=15,y=root_height-90)
+    score_text = str(gui_obj.grade) + ' correct out of ' + str(gui_obj.attempts) + ' attempts.'
+    score_label = tk.Label(root, text=score_text, width=25, wraplength=200)
+    score_label.place(x=15, y=root_height - 90)
 
     # next/previous/random buttons
     npr_frame = tk.Frame(root)
     npr_padx = 3
 
-    prev_button = tk.Button(npr_frame, text='Previous', command=partial(prev_gal, gui_obj, gal_objs, link_df, desc_df, image_label,info_label,galaxy_option,options))
-    prev_button.grid(row=0, column=0, padx = npr_padx)
+    prev_button = tk.Button(npr_frame, text='Previous', command=partial(prev_gal, gui_obj, gal_objs, link_df, desc_df, image_label, info_label, galaxy_option, options))
+    prev_button.grid(row=0, column=0, padx=npr_padx)
 
-    rand_button = tk.Button(npr_frame, text='Random', command=partial(rand_gal, gui_obj, gal_objs, link_df, desc_df, image_label,info_label,galaxy_option,options))
-    rand_button.grid(row=0, column=1, padx = npr_padx)
+    rand_button = tk.Button(npr_frame, text='Random', command=partial(rand_gal, gui_obj, gal_objs, link_df, desc_df, image_label, info_label, galaxy_option, options))
+    rand_button.grid(row=0, column=1, padx=npr_padx)
 
-    next_button = tk.Button(npr_frame, text='Next', command=partial(next_gal, gui_obj, gal_objs, link_df, desc_df, image_label,info_label,galaxy_option,options))
-    next_button.grid(row=0, column=2, padx = npr_padx)
+    next_button = tk.Button(npr_frame, text='Next', command=partial(next_gal, gui_obj, gal_objs, link_df, desc_df, image_label, info_label, galaxy_option, options))
+    next_button.grid(row=0, column=2, padx=npr_padx)
 
     # center next/prev/rand button frame below galaxy image
     npr_frame.place(x=0, y=0)
@@ -245,7 +244,7 @@ def main():
                     y=image_label_y_offset + image_label_height + (bottom_margin_height - npr_frame_height)/4)
     
     # spiral/elliptical buttons
-    button_frame = tk.Frame(root)#, bg='green')
+    button_frame = tk.Frame(root)
     spi_ell_pady = 5
 
     spiral_button = tk.Button(button_frame, text='Spiral', command=partial(spiral_func, gui_obj,gal_objs,info_label,score_label))
@@ -269,6 +268,9 @@ def main():
     #print(gui_obj.which_gal)
 
     #print(gal_objs[gui_obj.which_gal].choice)
+
+def main():
+    setup_gui()
 
 if __name__ == '__main__':
     main()
